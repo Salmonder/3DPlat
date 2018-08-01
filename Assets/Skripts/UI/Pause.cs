fff7d18ad0b04e7fb808e8a665745da074d01366
Pate <Paulus.ylimaki@gmail.com>
2018-08-01 17:40:00 +0300

    Pausemenu
    tasosta poistuminen main menuun

---
+++ Assets/Skripts/UI/Pause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    bool paused = false;
+    public GameObject PauseMenu;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+            OpenPauseMenu();
+        }
+    }
+    void OpenPauseMenu()
+    {
+        if (paused)
+        {
+            PauseMenu.SetActive(true);
+        }
+        else
+        {
+            PauseMenu.SetActive(false);
+        }
+
+    }
+}
