e7246c4c4e8c80d7b272c7e6003c979d689f571f
Salmonella <samuel.ylimaki@gmail.com>
2018-08-02 10:36:55 +0300

    Kamera
    kamera liikkuu nyt hitaammin y akselilla ja kääntyy katsomaan pelaajaa

--- Assets/Skripts/Player/CameraController.cs
+++ Assets/Skripts/Player/CameraController.cs
@@ -6,16 +6,26 @@ public class CameraController : MonoBehaviour {
 
     GameObject player;
     Vector3 offset;
+    Vector3 startPos;
+    Vector3 start2Now;
+    public float smooth = 0.2f;
 
-	// Use this for initialization
+
 	void Start () {
         player = GameObject.FindWithTag("Player");
         offset = transform.position - player.transform.position;
+        startPos = player.transform.position;
              
 	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
-        transform.position = player.transform.position + offset;
+
+    private void Update()
+    {
+        start2Now = player.transform.position - startPos;
+    }
+
+
+    void LateUpdate () {
+        transform.position = (player.transform.position + offset) - new Vector3(0,start2Now.y * smooth,0);
+        transform.LookAt(player.transform);
 	}
 }
