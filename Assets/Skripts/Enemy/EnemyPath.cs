6ea101f3abfc377506fa5cdb7200f442b4521856
Salmonella <samuel.ylimaki@gmail.com>
2018-08-12 22:08:29 +0300

    pupun model ja skripti fix
    skripti pupllu siistitty ja alotettu parannuksia
    
    modeli animointivalmis

--- Assets/Skripts/Enemy/EnemyPath.cs
+++ Assets/Skripts/Enemy/EnemyPath.cs
@@ -5,35 +5,49 @@ using UnityEngine;
 public class EnemyPath : MonoBehaviour {
 
     public Transform[] path;
-    public float speed;
-    public float reachDistance;
-    public int currentPoint;
+    int reachDistance = 1;
+    int currentPoint;                //polulle
 
+
+    public int speed =4;
+    public int chargeSpeed = 6;
+    Rigidbody rb;
+    float liikeX;
+    float liikeZ;                   //nopeus
+
+
+    Vector3 suunta;
+    Vector3 suuntaNorm;
     Vector3 dir;
     Vector3 ylös = Vector3.up;
-    public float turnSmooth;
+    public float turnSmooth;        //kääntyminen
 
     float dist2Player;
     Transform player;
-    public float sight = 5;
-    Rigidbody rb;
-    Vector3 suunta;
-    float liikeX;
-    float liikeZ;
+    public float sight = 5;     //etäisyys pelaajaan / Näkökenttä
+    
+
+    int moveMode; 
+    // 1 = menee Waypointille 2 = pysähtyy waypointilla 3 = näkee pelaajan 4 = juoksee kohti pelaajaa 5 = knocback
+
+   
+    
 
 
     private void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
-        rb = GetComponent<Rigidbody> ();
+        rb = GetComponent<Rigidbody> ();                       
     }
 
 
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(suunta.x * speed, rb.velocity.y, suunta.z * speed);
-       
+        if (suunta.magnitude >= 0.1)
+        {
+            rb.velocity = new Vector3(suuntaNorm.x * speed, rb.velocity.y, suuntaNorm.z * speed);       //normi liike
+        }
     }
 
 
@@ -44,25 +58,19 @@ public class EnemyPath : MonoBehaviour {
         float dist= Vector3.Distance(path [currentPoint].position, transform.position);
 
         suunta = new Vector3(dir.x, 0, dir.z);
+        suuntaNorm = suunta.normalized;                                                                 //Suuntien määritys
+
+
 
         if (dist2Player > sight)
-        {
-            //transform.position = Vector3.MoveTowards(transform.position, path[currentPoint].position, Time.deltaTime * speed);
-            dir = path[currentPoint].position - transform.position;
-
-
-
+        {            
+            dir = path[currentPoint].position - transform.position;            
         }
-
         if (dist2Player <= sight)
-        {
-            //transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime * speed);
+        {            
             dir = player.position - transform.position;
-        }
+        }                                                                                              // Määritetään Target (pelaaja vai waypoint)              
 
-        
-        
-            suunta = suunta.normalized;
         
 
         if (dist <=reachDistance)
@@ -71,30 +79,14 @@ public class EnemyPath : MonoBehaviour {
         }
         if (currentPoint >= path.Length)
         {
-            currentPoint = 0;
-        }
-
-
+            currentPoint = 0;            
+        }                                                                                              // Siirtää Waypointin seuraavaan
 
         
-        if (suunta != Vector3.zero)
+        if (suunta.magnitude >= 0.1)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(suunta, ylös), turnSmooth);
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        for(int i = 0; i < path.Length; i++)
-        {
-            if (path.Length > 0)
-            {
-                if (path[i] != null)
-                {
-                    Gizmos.DrawSphere(path[i].position, reachDistance);
-                }
-            }
-        }
+        }                                                                                               // Kääntäminen
     }
 
 }
